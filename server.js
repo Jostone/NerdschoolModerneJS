67c7bdc299a622f714e01f22849f2aee24848a4e
Jostein <jostone12@gmail.com>
2016-11-01 19:35:40 +0100

    Enkel rest server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
 
-
-app.get('/',(request,response) => response.send('Hello World'));
+app.use(morgan('dev'));
 
 app.listen(port,() => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get('/tvshow/',(request,response)=>response.send('All tv-shows'));
+
+app.get('/tvshow/:id',(request,response)=>response.send(`A show with id = ${request.params.id}`));
+
+app.post('/tvshow/',(request,response)=>response.send('Show was created'));
+
+app.delete('/tvshow/:id',(request,response)=>response.send(`Show ${request.params.id} was deleted.`));
+
+app.put('/tvshow/:id',(request,response)=>response.send(`Show ${request.params.id} was updated.`));
